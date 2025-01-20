f1d18db172dbc70841319ecd31dea4c82d464c6f
Neel449 <neelmanikumar449@gmail.com>
2025-01-20 10:36:03 +0530

    completed pin property of gsap

---
+++ Project-4(pin)/script.js
@@ -0,0 +1,13 @@
+gsap.to(".page2 h1", {
+    transform: "translateX(-140%)",
+    scrollTrigger: {
+        trigger: ".page2",
+        scroller: "body",
+        markers: "true",
+        start: "top 0%",
+        end: "top -200%",
+        scrub: 1,
+        pin: true,
+        // pin: 
+    }
+})
